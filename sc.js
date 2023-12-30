fc4592ba5ee194d7826fecac3f6aa293a1fbc45f
achennakeshavareddy1301 <125818932+achennakeshavareddy1301@users.noreply.github.com>
2023-12-30 07:57:28 +0530

    Update sc.js

---
+++ sc.js
@@ -0,0 +1,12 @@
+function visitpage1(){
+    window.location='https://leetcode.com/user4040BY/';
+}
+function visitpage2(){
+    window.location='https://github.com/achennakeshavareddy1301';
+}
+function visitpage3(){
+    window.location='https://auth.geeksforgeeks.org/user/ayyalurichennake36h6/?utm_source=geeksforgeeks&utm_medium=my_profile&utm_campaign=auth_user';
+}
+function visitpage4(){
+    window.location='https://achennakeshavareddy1301.github.io/spotify/';
+}
